498350743fb44cd56ca3ccaa724db5a9eb6616f7
Michael Persson <Michael.Persson@imc.nl>
2015-08-24 16:01:03 +0000

    Added driver and version

--- netinfo/netinfo.go
+++ netinfo/netinfo.go
@@ -1,19 +1,30 @@
 package netinfo
 
 import (
+	"github.com/mickep76/hwinfo/common"
 	"net"
+	"runtime"
 )
 
 // Info structure for information about a systems memory.
 type Info struct {
-	Name   string   `json:"name"`
-	MTU    int      `json:"mtu"`
-	IPAddr []string `json:"ipaddr"`
-	HWAddr string   `json:"hwaddr"`
+	Name            string   `json:"name"`
+	MTU             int      `json:"mtu"`
+	IPAddr          []string `json:"ipaddr"`
+	HWAddr          string   `json:"hwaddr"`
+	Driver          string   `json:"driver,omitempty"`
+	DriverVersion   string   `json:"driver_version,omitempty"`
+	FirmwareVersion string   `json:"firmware_version,omitempty"`
 }
 
 // GetInfo return information about a systems memory.
 func GetInfo() ([]Info, error) {
+	fields := []string{
+		"driver",
+		"version",
+		"firmware-version",
+	}
+
 	i := []Info{}
 
 	ifs, err := net.Interfaces()
@@ -36,12 +47,30 @@ func GetInfo() ([]Info, error) {
 			ia = append(ia, addr.String())
 		}
 
-		i = append(i, Info{
-			Name:   v.Name,
-			HWAddr: v.HardwareAddr.String(),
-			MTU:    v.MTU,
-			IPAddr: ia,
-		})
+		switch runtime.GOOS {
+		case "linux":
+			o, err := common.ExecCmdFields("/usr/sbin/ethtool", []string{"-i", v.Name}, ":", fields)
+			if err != nil {
+				return []Info{}, err
+			}
+
+			i = append(i, Info{
+				Name:            v.Name,
+				HWAddr:          v.HardwareAddr.String(),
+				MTU:             v.MTU,
+				IPAddr:          ia,
+				Driver:          o["driver"],
+				DriverVersion:   o["version"],
+				FirmwareVersion: o["firmware-version"],
+			})
+		case "darwin":
+			i = append(i, Info{
+				Name:   v.Name,
+				HWAddr: v.HardwareAddr.String(),
+				MTU:    v.MTU,
+				IPAddr: ia,
+			})
+		}
 	}
 
 	return i, nil
