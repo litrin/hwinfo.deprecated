4e9901bed4c5a81b12b2aac2b07d2bf7e45b82dc
Michael Persson <michael.ake.persson@gmail.com>
2015-08-26 09:11:32 +0000

    Include class name

--- pciinfo/pciinfo.go
+++ pciinfo/pciinfo.go
@@ -13,7 +13,8 @@ type PCI struct {
 	BusNum      string `json:"bus_number"`
 	DeviceNum   string `json:"device_number"`
 	DeviceFunc  string `json:"device_function"`
-	Class       string `json:"class"`
+	ClassID     string `json:"class_id"`
+	ClassName   string `json:"class_name"`
 	VendorID    string `json:"vendor_id"`
 	DeviceID    string `json:"device_id"`
 	SubVendorID string `json:"subsys_vendor_id"`
@@ -27,6 +28,22 @@ type PCI struct {
 type Info struct {
 	PCI []PCI `json:"pci"`
 }
+
+/*
+# Syntax:
+# vendor  vendor_name
+#       device  device_name                             <-- single tab
+#               subvendor subdevice  subsystem_name     <-- two tabs
+
+0010  Allied Telesis, Inc (Wrong ID)
+# This is a relabelled RTL-8139
+        8139  AT-2500TX V3 Ethernet
+001c  PEAK-System Technik GmbH
+        0001  PCAN-PCI CAN-Bus controller
+                001c 0004  2 Channel CAN Bus SJC1000
+                001c 0005  2 Channel CAN Bus SJC1000 (Optically Isolated)
+...
+*/
 
 // TODO: Cache PCI database as a map[string]string
 func getPCIVendor(vendorID string, deviceID string, subsysVendorID string, subsysDeviceID string) (string, string, string, error) {
@@ -60,12 +77,6 @@ func getPCIVendor(vendorID string, deviceID string, subsysVendorID string, subsy
 			vals[i] = strings.Trim(vals[i], " \t")
 		}
 
-		// Does it need \t
-		/*
-			id := strings.Trim(vals[0], " \t")
-			val := strings.Trim(vals[1], " ")
-		*/
-
 		if strings.LastIndex(line, "\t") == -1 && vals[0] == vendorID {
 			vendor = vals[1]
 			continue
@@ -88,6 +99,89 @@ func getPCIVendor(vendorID string, deviceID string, subsysVendorID string, subsy
 	}
 
 	return vendor, device, subsysName, nil
+}
+
+/*
+# List of known device classes, subclasses and programming interfaces
+
+# Syntax:
+# C class       class_name
+#       subclass        subclass_name           <-- single tab
+#               prog-if  prog-if_name   <-- two tabs
+
+C 01  Mass storage controller
+        00  SCSI storage controller
+        01  IDE interface
+        02  Floppy disk controller
+        03  IPI bus controller
+        04  RAID bus controller
+        05  ATA controller
+                20  ADMA single stepping
+                30  ADMA continuous operation
+...
+*/
+
+// TODO: Cache PCI database as a map[string]string
+func getPCIClass(classID string) (string, string, string, error) {
+	if len(classID) < 8 {
+		return "", "", "", fmt.Errorf("Class string is to short: %s", classID)
+	}
+
+	subClassID := classID[4:6]
+	progIntfID := classID[6:8]
+	classID = classID[2:4]
+
+	fn := "/usr/share/hwdata/pci.ids"
+	if _, err := os.Stat(fn); os.IsNotExist(err) {
+		return "", "", "", fmt.Errorf("file doesn't exist: %s", fn)
+	}
+
+	o, err := ioutil.ReadFile(fn)
+	if err != nil {
+		return "", "", "", err
+	}
+
+	class := ""
+	subClass := ""
+	progIntf := ""
+	begins := false
+	for _, line := range strings.Split(string(o), "\n") {
+		if !begins && strings.HasPrefix(line, "C 01  Mass storage controller") {
+			begins = true
+		} else if !begins || strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		vals := strings.SplitN(strings.Replace(line, "C ", "", 1), " ", 2)
+		if len(vals) < 2 {
+			continue
+		}
+
+		for i := 0; i < 2; i++ {
+			vals[i] = strings.Trim(vals[i], " \t")
+		}
+
+		if strings.LastIndex(line, "\t") == -1 && vals[0] == classID {
+			class = vals[1]
+			continue
+		}
+
+		if class != "" && strings.LastIndex(line, "\t") == 0 && vals[0] == subClassID {
+			subClass = vals[1]
+			continue
+		}
+
+		if class != "" && subClass != "" && strings.LastIndex(line, "\t") == 1 && vals[0] == progIntfID {
+			progIntf = vals[1]
+			break
+		}
+
+		if class != "" && strings.LastIndex(line, "\t") == -1 {
+			break
+		}
+	}
+
+	return class, subClass, progIntf, nil
 }
 
 // GetInfo return information about PCI devices.
@@ -114,17 +208,30 @@ func GetInfo() (Info, error) {
 			return Info{}, err
 		}
 
+		classID := o["class"]
+		class, subClass, _, err := getPCIClass(classID)
+		if err != nil {
+			return Info{}, err
+		}
+		if subClass != "" {
+			class = subClass
+		}
+
 		vendorID := o["vendor"]
 		deviceID := o["device"]
 		subVendorID := o["subsystem_vendor"]
 		subDeviceID := o["subsystem_device"]
 		vendor, device, subsysName, err := getPCIVendor(vendorID, deviceID, subVendorID, subDeviceID)
+		if err != nil {
+			return Info{}, err
+		}
 
 		i.PCI = append(i.PCI, PCI{
 			BusNum:      pci[1],
 			DeviceNum:   dev[0],
 			DeviceFunc:  dev[1],
-			Class:       o["class"],
+			ClassID:     classID,
+			ClassName:   class,
 			VendorID:    vendorID,
 			DeviceID:    deviceID,
 			SubDeviceID: subDeviceID,
