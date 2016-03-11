f489d2ec062bcb429b769e20a349fa45bb209f61
Michael Persson <michael.ake.persson@gmail.com>
2016-03-11 15:13:19 -0600

    Fixed dock2box

--- dock2box/dock2box.go
+++ dock2box/dock2box.go
@@ -8,9 +8,16 @@ import (
 	"path"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
-type Layer struct {
+type Dock2box interface {
+	SetTTL(int)
+	Get() error
+	Refresh() error
+}
+
+type layer struct {
 	Layer             string `json:"layer"`
 	Image             string `json:"image"`
 	Repo              string `json:"repo"`
@@ -24,54 +31,93 @@ type Layer struct {
 	CFlagsMarchNative string `json:"cflags_march_native"`
 }
 
-type Dock2Box struct {
-	FirstBoot string  `json:"firstboot"`
-	CFlags    string  `json:"cflags_march_native"`
-	Layers    []Layer `json:"layers"`
+type dock2box struct {
+	FirstBoot string    `json:"firstboot"`
+	CFlags    string    `json:"cflags_march_native"`
+	Layers    []layer   `json:"layers"`
+	Last      time.Time `json:"last"`
+	TTL       int       `json:"ttl_sec"`
+	Fresh     bool      `json:"fresh"`
 }
 
-// Get information about Dock2Box image layers.
-func Get() (Dock2Box, error) {
+// New constructor.
+func New() *dock2box {
+	return &dock2box{
+		TTL: 12 * 60 * 60,
+	}
+}
+
+// Get info.
+func (d *dock2box) Get() error {
+	if d.Last.IsZero() {
+		if err := d.Refresh(); err != nil {
+			return err
+		}
+	} else {
+		expire := d.Last.Add(time.Duration(d.TTL) * time.Second)
+		if expire.Before(time.Now()) {
+			if err := d.Refresh(); err != nil {
+				return err
+			}
+		} else {
+			d.Fresh = false
+		}
+	}
+
+	return nil
+}
+
+// Refresh cache.
+func (d *dock2box) Refresh() error {
+	if err := d.get(); err != nil {
+		return err
+	}
+	d.Last = time.Now()
+	d.Fresh = true
+
+	return nil
+}
+
+func (d2b *dock2box) get() error {
 	file := "/etc/dock2box/firstboot.json"
 	if _, err := os.Stat(file); os.IsNotExist(err) {
-		return Dock2Box{}, fmt.Errorf("file doesn't exist: %s", file)
+		return fmt.Errorf("file doesn't exist: %s", file)
 	}
 
 	o, err := ioutil.ReadFile(file)
 	if err != nil {
-		return Dock2Box{}, err
+		return err
 	}
 
-	d2b := Dock2Box{}
 	if err := json.Unmarshal(o, &d2b); err != nil {
-		return Dock2Box{}, err
+		return err
 	}
 
 	files, err := filepath.Glob("/etc/dock2box/layers/*.json")
 	if err != nil {
-		return Dock2Box{}, err
+		return err
 	}
 
 	for _, file := range files {
 		if _, err := os.Stat(file); os.IsNotExist(err) {
-			return Dock2Box{}, fmt.Errorf("file doesn't exist: %s", file)
+			return fmt.Errorf("file doesn't exist: %s", file)
 		}
 
 		o, err := ioutil.ReadFile(file)
 		if err != nil {
-			return Dock2Box{}, err
+			return err
 		}
 
-		layer := Layer{}
-		if err := json.Unmarshal(o, &layer); err != nil {
-			return Dock2Box{}, err
+		l := layer{}
+		if err := json.Unmarshal(o, &l); err != nil {
+			return err
 		}
 
 		fn := path.Base(file)
-		layer.Layer = strings.TrimSuffix(fn, filepath.Ext(fn))
+		l.Layer = strings.TrimSuffix(fn, filepath.Ext(fn))
 
-		d2b.Layers = append(d2b.Layers, layer)
+		d2b.Layers = append(d2b.Layers, l)
 	}
 
-	return d2b, nil
+	return nil
 }
