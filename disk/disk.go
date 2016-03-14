898020c9839fc247b4a4ea8d7847394ca935427d
Michael Persson <michael.ake.persson@gmail.com>
2016-03-14 14:16:40 -0500

    Fix disk ;

--- disk/disk.go
+++ disk/disk.go
@@ -1,62 +1,81 @@
+// +build linux
+
 package disk
 
 import (
+	"path/filepath"
+	"strconv"
 	"time"
+
+	"github.com/mickep76/hwinfo/common"
 )
 
-type Disk interface {
-	SetTTL(int)
+type Disks interface {
 	Get() error
-	Refresh() error
 }
+
+type Cached interface {
+	SetTimeout(int)
+	Get() error
+	GetRefresh() error
+}
+
+type disks []disk
 
 type disk struct {
-	Devices []device  `json:"devices"`
-	Last    time.Time `json:"last"`
-	TTL     int       `json:"ttl_sec"`
-	Fresh   bool      `json:"fresh"`
-}
-
-type device struct {
 	Device string `json:"device"`
 	Name   string `json:"name"`
+	SizeKB int    `json:"size_kb"`
 	SizeGB int    `json:"size_gb"`
 }
 
-// New constructor.
-func New() *disk {
-	return &disk{
-		TTL: 5 * 60,
+type cached struct {
+	Disks       *disks    `json:"disks"`
+	LastUpdated time.Time `json:"last_updated"`
+	Timeout     int       `json:"timeout_sec"`
+	FromCache   bool      `json:"from_cache"`
+}
+
+func New() *disks {
+	return &disks{}
+}
+
+func NewCached() *cached {
+	return &cached{
+		Disks:   New(),
+		Timeout: 5 * 60, // 5 minutes
 	}
 }
 
-// Get info.
-func (d *disk) Get() error {
-	if d.Last.IsZero() {
-		if err := d.Refresh(); err != nil {
-			return err
-		}
-	} else {
-		expire := d.Last.Add(time.Duration(d.TTL) * time.Second)
-		if expire.Before(time.Now()) {
-			if err := d.Refresh(); err != nil {
-				return err
-			}
-		} else {
-			d.Fresh = false
-		}
-	}
-
-	return nil
-}
-
-// Refresh cache.
-func (d *disk) Refresh() error {
-	if err := d.get(); err != nil {
+func (disks *disks) Get() error {
+	files, err := filepath.Glob("/sys/class/block/*")
+	if err != nil {
 		return err
 	}
-	d.Last = time.Now()
-	d.Fresh = true
+
+	for _, path := range files {
+		o, err := common.LoadFiles([]string{
+			filepath.Join(path, "dev"),
+			filepath.Join(path, "size"),
+		})
+		if err != nil {
+			return err
+		}
+
+		d := disk{}
+
+		d.Name = filepath.Base(path)
+		d.Device = o["dev"]
+
+		d.SizeKB, err = strconv.Atoi(o["size"])
+		if err != nil {
+			return err
+		}
+		d.SizeKB = d.SizeKB * 512 / 1024
+		d.SizeGB = d.SizeKB / 1024 / 1024
+
+		*disks = append(*disks, d)
+	}
 
 	return nil
 }
